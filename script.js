a5c1ea2dbeb74ac661bc0d737e976463a267f2b4
nnapolitx <nnapolitano88@gmail.com>
2022-08-22 14:58:10 -0400

    selected buttons in js

--- script.js
+++ script.js
@@ -38,3 +38,31 @@ function operate(a, operator, b) {
     return operator(a,b);
 }
 
+//selecting and assigning operator buttons
+const clear = document.querySelector('.clear');
+const negativeNumber = document.querySelector('.posNeg');
+const percent = document.querySelector('.percent');
+const divide = document.querySelector('.divide');
+const multiply = document.querySelector('.multiply');
+const minus = document.querySelector('.minus');
+const plus = document.querySelector('.plus');
+const power = document.querySelector('.power');
+const decimal = document.querySelector('.decimal');
+const equals = document.querySelector('.equals');
+
+//selecting and assigning number buttons
+const one = document.querySelector('.one');
+const two = document.querySelector('.two');
+const three = document.querySelector('.three');
+const four = document.querySelector('.four');
+const five = document.querySelector('.five');
+const six = document.querySelector('.six');
+const seven = document.querySelector('.seven');
+const eight = document.querySelector('.eight');
+const nine = document.querySelector('.nine');
+const zero = document.querySelector('.zero');
+
+//populate display and store value for operator function
+const CalculatorDisplay = document.querySelector('.display');
+
+
