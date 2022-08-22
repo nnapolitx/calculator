c737bf8043b61ffd752f02af801a04bd273ff1e4
nnapolitx <nnapolitano88@gmail.com>
2022-08-22 17:11:59 -0400

    Now holds and displays a variable, still need to limit char; will save for the end

--- script.js
+++ script.js
@@ -39,45 +39,40 @@ function operate(a, operator, b) {
 }
 
 //selecting and assigning operator buttons
-const clear = document.querySelector('.clear');
-const negativeNumber = document.querySelector('.posNeg');
-const percent = document.querySelector('.percent');
-const divide = document.querySelector('.divide');
-const multiply = document.querySelector('.multiply');
-const minus = document.querySelector('.minus');
-const plus = document.querySelector('.plus');
-const power = document.querySelector('.power');
-const decimal = document.querySelector('.decimal');
-const equals = document.querySelector('.equals');
+//const clear = document.querySelector('.clear');
+//const negativeNumber = document.querySelector('.posNeg');
+//const percent = document.querySelector('.percent');
+//const divide = document.querySelector('.divide');
+//const multiply = document.querySelector('.multiply');
+//const minus = document.querySelector('.minus');
+//const plus = document.querySelector('.plus');
+//const power = document.querySelector('.power');
+//const decimal = document.querySelector('.decimal');
+//const equals = document.querySelector('.equals');
 
 //selecting and assigning number buttons
 const numberButton = document.querySelectorAll('.number');
-//const two = document.querySelector('.two');
-//const three = document.querySelector('.three');
-//const four = document.querySelector('.four');
-//const five = document.querySelector('.five');
-//const six = document.querySelector('.six');
-//const seven = document.querySelector('.seven');
-//const eight = document.querySelector('.eight');
-//const nine = document.querySelector('.nine');
-//const zero = document.querySelector('.zero');
 
 //populate display and store value for operator function
 const CalculatorDisplay = document.querySelector('.display');
 
 let currentVarA = '';
 let currentVarB = '';
+let currentOperator = '';
+let newOperator ='';
 
 numberButton.forEach(index => {
     index.addEventListener('click', (e) => {
+        if (currentOperator.length<1){
         console.log(e.target.innerText);
         currentVarA += e.target.innerText;
-    })
-})
-
-//numBtnArr.addEventListener('click', function(i) {
-//    currentVarA += one.innerText;
-//    CalculatorDisplay.textContent = `${currentVarA}`;
-//});
-
+        CalculatorDisplay.textContent = currentVarA;
+        }
+        else if (currentOperator.length>1){
+            console.log(e.target.innerText);
+            currentVarB += e.target.innerText;
+            CalculatorDisplay.textContent = currentVarB;
+        }
+    });
+});
 
