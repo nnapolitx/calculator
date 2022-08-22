2d3eee0fbcbe122900a29d620a7af8be26f0eae6
nnapolitx <nnapolitano88@gmail.com>
2022-08-22 12:31:37 -0400

    functions for basic operations

---
+++ script.js
@@ -0,0 +1,35 @@
+//Functions for the calculator will go here
+
+function addition (a, b) {
+    let sum = a + b;
+    return sum;
+}
+
+function subtraction (a, b) {
+    let difference = a - b;
+    return difference;
+}
+
+function multiplication (a, b) {
+    let product = a*b;
+    return product;
+}
+
+function division (a, b) {
+    if (b === 0){
+        return 'Error, cannot divide by zero.'
+    }
+    else{
+        let product = a/b;
+        return product;
+    }
+}
+
+function powerOfInt (a, b) {
+    let power = a;
+    for (i = 1; i<b; i++){
+        power*=a;
+    }
+    return power;
+}
+
